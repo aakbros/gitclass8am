309885bc401155f9e635e25f204341d874941f73
aakbros <aravindkumaranandan@gmail.com>
2022-01-09 18:55:48 +0530

    taskdone

--- src/org/git/Employee.java
+++ src/org/git/Employee.java
@@ -1,5 +1,16 @@
 package org.git;
 
 public class Employee {
-
+	
+	private void empId(){
+		
+		
+		System.out.println("empid is 123");
+	}
+	
+	public static void main(String[] args) {
+Employee e = new Employee();
+e.empId();
+	
+	}
 }
